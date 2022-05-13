0ed2102f8e31e9c5e8eef5107173071730e2b3c7
pooni01 <1018451814@qq.com>
2022-05-13 18:34:44 +0800

    feat(*): 初始化项目

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Search from './search/index.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Search></Search>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/search/index.tsx
@@ -0,0 +1,9 @@
+const Search = () => {
+    return (
+      <div className="App">
+        <h1>1</h1>
+      </div>
+    );
+  }
+  
+  export default Search;
